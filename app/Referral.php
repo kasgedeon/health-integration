7f2f5aeee2c282661f2c494b00dc6a77bd0887db
kasgedeon <kas.gedeon20@gmail.com>
2018-09-20 10:46:15 +0300

    Patient Referral Relation

--- app/Referral.php
+++ app/Referral.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Referral extends Model
 {
-    //
+    // Relation to Patient
+    public function patient(){
+        return $this->belongsTo('App\Patient', 'patient_id')->withDefault([
+            'id' => 0,
+            'name' => 'Unknown Hospital',
+            'ref_no' => 'Unknown Reference',
+            'type' => 'Unknown Hospital Type',
+            'address' => 'Unkonwn Address',
+            'website' => 'Unknwon Website',
+            'mobile' => 'Unknown Mobile'
+        ]);
+    }
 }
